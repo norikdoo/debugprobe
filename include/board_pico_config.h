4c4b5b02a73a26342f1d0eac8c9a00d1ddbda928
Florijan Plohl <florijan.plohl@norik.com>
2026-06-09 09:00:36 +0200

    include: Configure RP2040 pins for DECT NR+ USB Dongle
    Configures the RP2040 pins based on schematics from DECT NR+ USB Dongle.
    
    Signed-off-by: Florijan Plohl <florijan.plohl@norik.com>

--- include/board_pico_config.h
+++ include/board_pico_config.h
@@ -31,20 +31,20 @@
 
 // PIO config
 #define PROBE_SM 0
-#define PROBE_PIN_OFFSET 2
-#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 0) // 2
-#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 1) // 3
+#define PROBE_PIN_OFFSET 11
+#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 0) // 11
+#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 1) // 12
 // Target reset config
-#define PROBE_PIN_RESET 1
+#define PROBE_PIN_RESET 15
 
 // UART config
-#define PROBE_UART_TX 4
-#define PROBE_UART_RX 5
+#define PROBE_UART_TX 17 // Switched around from compared to schematics
+#define PROBE_UART_RX 16 // Because TX from RP2040 needs to go to RX on nRF9151
 #define PROBE_UART_INTERFACE uart1
 #define PROBE_UART_BAUDRATE 115200
 
-#define PROBE_USB_CONNECTED_LED 25
+#define PROBE_USB_CONNECTED_LED 2
 
-#define PROBE_PRODUCT_STRING "Debugprobe on Pico (CMSIS-DAP)"
+#define PROBE_PRODUCT_STRING "Debugprobe on DECT NR+ USB Dongle (CMSIS-DAP)"
 
 #endif
